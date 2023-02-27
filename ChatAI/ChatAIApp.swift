4c2f3f8b939c94f48ad3d9207ea9f963766b6170
John Reichel <jreichel447@gmail.com>
2023-02-27 14:12:41 -0600

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by John Reichel on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
